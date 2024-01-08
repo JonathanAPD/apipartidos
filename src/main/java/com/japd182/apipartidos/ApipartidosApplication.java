35777f3ec10b4272084f6b83e37528fa90307674
JonathanAPD <jpatino4@udi.edu.co>
2024-01-08 00:07:38 -0500

    carga de proyecto initializr

---
+++ src/main/java/com/japd182/apipartidos/ApipartidosApplication.java
@@ -0,0 +1,13 @@
+package com.japd182.apipartidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipartidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipartidosApplication.class, args);
+	}
+
+}
